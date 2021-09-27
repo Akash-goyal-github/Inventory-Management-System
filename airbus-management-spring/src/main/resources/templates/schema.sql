256ebe9d834c690e2d1c639dfefd00df6f3c408b
Akash Goyal <akashgoyal0401@gmail.com>
2021-09-27 20:22:05 +0530

    Update schema.sql

--- airbus-management-spring/src/main/resources/templates/schema.sql
+++ airbus-management-spring/src/main/resources/templates/schema.sql
@@ -1,7 +1,7 @@
-use product;
+use Product;
 
-drop table product;
-create table product(
+drop table Product;
+create table Product(
 productId varchar(256) Unique not null,
 productName varchar(256),
 productDescription varchar(3500),
@@ -16,6 +16,6 @@ password varchar(256)
 );
 
 
-use product;
-insert into user(username,password) Values("airbus01","$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6");
-insert into user(username,password) Values("airbus02","$2a$10$ZnnAdfh3cc7a/b1aODLeoOjifNPbHL6Vo8kpRJj.muPsVp1697hJO");
+use Product;
+insert into User(username,password) Values("airbus01","$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6");
+insert into User(username,password) Values("airbus02","$2a$10$ZnnAdfh3cc7a/b1aODLeoOjifNPbHL6Vo8kpRJj.muPsVp1697hJO");
